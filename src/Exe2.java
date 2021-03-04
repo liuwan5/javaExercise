0c5258f805a02ece3bea3217666366d763ecf5cd
yajia liu <201250115@smail.nju.edu.cn>
2021-03-04 17:55:50 +0800

    commit

---
+++ src/Exe2.java
@@ -0,0 +1,16 @@
+class Num{
+    Num(float f){
+        n=f;
+    }
+    float n;
+}
+public class Exe2 {
+    public static void main(String[] args) {
+        Num m=new Num(2.1f);
+        f(m);
+        System.out.println(m.n);
+    }
+    static void f(Num fl){
+        fl.n=0.9f;
+    }
+}
