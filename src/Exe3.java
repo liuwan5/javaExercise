08d102a41bb3dea555ae2c50891542db445b45a3
yajia liu <201250115@smail.nju.edu.cn>
2021-03-06 16:33:32 +0800

    commit

---
+++ src/Exe3.java
@@ -0,0 +1,8 @@
+public class Exe3 {
+    public static void main(String[] args) {
+        long l1=0xFFL;
+        long l2=076L;
+        System.out.println(Long.toBinaryString(l1));
+        System.out.println(Long.toBinaryString(l2));
+    }
+}
