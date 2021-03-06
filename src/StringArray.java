9292618f0a206aa657805edc61cfb937a7eb989c
yajia liu <201250115@smail.nju.edu.cn>
2021-03-06 16:48:39 +0800

    commit

---
+++ src/StringArray.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class StringArray {
+    public static void main(String[] args) {
+        String[] strs=new String[]{
+                "Liu",
+                "Ya",
+                "Jia"
+        };
+        for(String str:strs){
+            System.out.print(str+" ");
+        }
+        System.out.print('\n');
+        System.out.println(Arrays.toString(strs));
+    }
+}
